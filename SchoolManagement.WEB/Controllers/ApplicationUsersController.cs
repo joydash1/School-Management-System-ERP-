e601100a202055367e2001dfd2cf2cfcbda1ba5e
Nir jhor <nirjhor268@gmail.com>
2026-01-13 00:43:44 +0600

    Application User Controller

---
+++ SchoolManagement.WEB/Controllers/ApplicationUsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchoolManagement.WEB.Controllers
+{
+    public class ApplicationUsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
